509be84b168687c2d1b5161087e46471259e2f32
desaura <tanseersaji@gmail.com>
2016-10-17 20:15:27 +0530

    menu animation and links

--- js/main.js
+++ js/main.js
@@ -3,6 +3,32 @@ $("document").ready(function(){
         $('.menu').css("top","0%");
     });
     $('.close').on('click', function(){
-        $('.menu').css("top","-100%");
+        close_menu();       
     });
+});
+var close_menu = function(){
+    $('.menu').css("top","-110%");
+}
+$(window).scroll(function(){
+    var wscroll = $(this).scrollTop();
+     if(wscroll >= 230){
+         $('.menu-button').addClass('menu-button-bottom');
+     }
+     else{
+         $('.menu-button').removeClass('menu-button-bottom');
+     }
+});
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 20
+        }, 500);
+        return false;
+      }
+    }
+  });
 });
